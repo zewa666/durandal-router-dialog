f6bbeb2fbed0e02a807b83ff19a7fdbb77618760
Zewa <zewa666@gmail.com>
2014-10-11 13:53:34 +0200

    Generalized usage
    more general usage for the dialog creation through pub/sub

--- app/viewmodels/home.js
+++ app/viewmodels/home.js
@@ -1,20 +1,11 @@
 define(function(require) {
 
-  var app = require("durandal/app"),
-      dialog = require('plugins/dialog');
-
-
   var vm_home = function() {
     //Your code
   };
 
-  vm_home.prototype.activate = function(showModal) {
+  vm_home.prototype.activate = function() {
 
-    if(showModal !== undefined &&
-       showModal === 'true') {
-      console.log(showModal);
-      dialog.show('viewmodels/signin');
-    }
   };
 
   return vm_home;
--- app/viewmodels/shell.js
+++ app/viewmodels/shell.js
@@ -1,4 +1,4 @@
-﻿define(['plugins/router', 'durandal/app', 'knockout'], function (router, app, ko) {
+﻿define(['plugins/router', 'durandal/app', 'knockout', 'plugins/dialog'], function (router, app, ko, dialog) {
     return {
         router: router,
         activate: function () {
@@ -6,6 +6,15 @@
                 { route: '', icon: 'glyphicon glyphicon-home', title:'Startpage', moduleId: 'viewmodels/start', nav: true },
                 { route: 'home(/:showModal)', icon: 'glyphicon glyphicon-home', title:'Home', moduleId: 'viewmodels/home', nav: false }
             ]).buildNavigationModel();
+
+          router.on('router:navigation:complete').then(function(instance, instruction, router){
+            console.log(instruction);
+              if(
+                instruction.fragment !== '' &&
+                instruction.fragment.indexOf('signInModal') >= 0) {
+                dialog.show('viewmodels/signin');
+              }
+          });
 
             return router.activate();
         },
