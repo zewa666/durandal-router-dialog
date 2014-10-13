256f7ceab1b0fb1fa3ac9880049e256fc6ef7ac6
Zewa <zewa666@gmail.com>
2014-10-13 19:28:43 +0200

    Inspect unknown route and reroute to modal if condition met

--- app/viewmodels/shell.js
+++ app/viewmodels/shell.js
@@ -1,27 +1,36 @@
 ﻿define(['plugins/router', 'durandal/app', 'knockout', 'plugins/dialog'], function (router, app, ko, dialog) {
-    return {
-        router: router,
-        activate: function () {
+  return {
+    router: router,
+    activate: function () {
 
-          router.map([
-            { route: '', icon: 'glyphicon glyphicon-home', title:'Startpage', moduleId: 'viewmodels/start', nav: true },
-            { route: 'home(/:showModal)', icon: 'glyphicon glyphicon-home', title:'Home', moduleId: 'viewmodels/home', nav: false },
-            { route: 'about(/:showModal)', icon: 'glyphicon glyphicon-home', title:'About', moduleId: 'viewmodels/about', nav: false }
-          ]).buildNavigationModel();
+      router.map([
+        { route: '', icon: 'glyphicon glyphicon-home', title: 'Startpage', moduleId: 'viewmodels/start', nav: true },
+        { route: 'home(/:showModal)', icon: 'glyphicon glyphicon-home', title: 'Home', moduleId: 'viewmodels/home', nav: false },
+        { route: 'about(/:showModal)', icon: 'glyphicon glyphicon-home', title: 'About', moduleId: 'viewmodels/about', nav: false }
+      ]).buildNavigationModel();
 
-          router.on('router:navigation:complete').then(function(instance, instruction, router){
-            console.log(instruction);
-              if(
-                instruction.fragment !== '' &&
-                instruction.fragment.indexOf('signInModal') >= 0) {
-                dialog.show('viewmodels/signin');
-              }
-          });
-
-            return router.activate();
-        },
-        displayNavItem: function(item) {
-          return "<i class='" + item.icon + "'> </i> " + item.title;
+      router.on('router:navigation:complete').then(function (instance, instruction, router) {
+        console.log(instruction);
+        if (
+          instruction.fragment !== '' &&
+          instruction.fragment.indexOf('signInModal') >= 0) {
+          dialog.show('viewmodels/signin');
         }
-    };
+      });
+
+      router.mapUnknownRoutes(function(instruction) {
+        if (instruction.fragment !== '' &&
+            instruction.fragment.indexOf('signin') >= 0) {
+          console.log(instruction);
+          instruction.config.moduleId = router.activeInstruction() !== null ? router.activeInstruction().config.moduleId : 'viewmodels/home';
+          dialog.show('viewmodels/signin');
+        }
+      });
+
+      return router.activate();
+    },
+    displayNavItem: function (item) {
+      return "<i class='" + item.icon + "'> </i> " + item.title;
+    }
+  };
 });
